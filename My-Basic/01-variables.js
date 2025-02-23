e70a20ad01559843da3dc5569eb09c45ad1adabe
rafatoski <rafatoski@hotmail.com>
2025-02-23 17:55:44 -0500

    Calse 2: Variables en Javascript

--- My-Basic/01-variables.js
+++ My-Basic/01-variables.js
@@ -1,2 +1,17 @@
+// var
+
 var helloWorld = "hello, Javascript!"
 console.log(helloWorld)
+
+helloWorld = "Hola de nuevo, Javascript!"
+console.log(helloWorld)
+
+// let
+
+let helloWorld2 = "hola, javascript 2"
+console.log(helloWorld2)
+
+// const
+
+const helloWorld3= "¡Hola javascript 3!"
+console.log(helloWorld3)
