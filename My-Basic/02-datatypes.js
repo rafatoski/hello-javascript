bda12ff4b0a4ac7d75250db91ccb08fde8439b13
rafatoski <rafatoski@hotmail.com>
2025-02-23 18:23:17 -0500

    Clase 3: tipo de datos

---
+++ My-Basic/02-datatypes.js
@@ -0,0 +1,44 @@
+// Cadena de texto (string)
+let myName = "Rafael Castillo"
+let alias = 'Rafatoski'
+let email = `rafaelcastillo@gmail.com`
+
+// Números (number)
+let age = 35
+let height = 1.75
+
+// Booleanos (boolean)
+let isStudent = true
+let isTeacher = false
+
+// Undefined
+let undefinedValue 
+
+// Null
+let nullValue = null
+
+// Symbol 
+let mySymbol = Symbol("mysymbol") // Symbol siempre empieza en maysucula
+
+//BigInt
+let myBigInt1 = BigInt(4563154612165646216345465165441641631616466161565461461616164561)
+let myBigInt2 = 3243541313654651564513n
+
+// mostramos tipo de datos
+console.log(typeof myName)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof height)
+
+console.log(typeof isStudent)
+console.log(typeof isTeacher)
+
+console.log(typeof undefinedValue)
+
+console.log(typeof nullValue)
+console.log(typeof mysymbol)
+
+console.log(typeof myBigInt1)
+console.log(typeof myBigInt2)
